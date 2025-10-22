8d1032e1597b6a5cd352037bc7972b124e57fe30
NFRamz <nframzi051@gmail.com>
2025-10-22 12:47:58 +0700

    First Commit

---
+++ js/Header.js
@@ -0,0 +1,7 @@
+    // --- Mobile Menu Toggle ---
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    mobileMenuButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
